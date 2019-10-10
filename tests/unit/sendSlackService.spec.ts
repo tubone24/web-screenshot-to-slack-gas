2ee73e6116942629ab300e1250f49f9dc8ae1d10
Yu Otsubo <uu-ootsubo@kddi.com>
2019-10-10 21:45:38 +0900

    test sendSlackService

--- tests/unit/sendSlackService.spec.ts
+++ tests/unit/sendSlackService.spec.ts
@@ -1,11 +1,18 @@
 import { SendSlackService } from '../../src/sendSlackService';
 
 // @ts-ignore
-GoogleAppsScript.URL_Fetch.UrlFetchApp.fetch = jest.fn(() => {});
+const mockFetch = jest.fn();
+UrlFetchApp.fetch = mockFetch;
 
 describe('sendSlackService', () => {
   it('sendImage', () => {
     const actual = SendSlackService.sendImage('test-token', 'test-image', 'test-title', '#test');
+    const expected_option = {
+      method: 'post',
+      payload: { token: 'test-token', file: 'test-image', channels: '#test', title: 'test-title' }
+    };
+    expect(mockFetch.mock.calls[0][0]).toBe('https://slack.com/api/files.upload');
+    expect(mockFetch.mock.calls[0][1]).toEqual(expected_option);
     expect(actual).toBe(true);
   });
 });
